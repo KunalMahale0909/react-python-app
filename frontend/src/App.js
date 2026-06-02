6dfb8bdf26b5ec87aa9e28e14e880d30d7dc98f0
KunalMahale0909 <mahaleku09@gmail.com>
2026-06-02 18:57:16 +0530

    Added React and Flask demo app

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>React Frontend</h1>
+      <h2>CI/CD Pipeline Demo</h2>
+      <p>GitHub → Jenkins → Harbor → Kubernetes</p>
+    </div>
+  );
+}
+
+export default App;
